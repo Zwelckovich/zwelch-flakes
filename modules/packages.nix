9989a44460846470cb10ebec9b0d82c24e563102
Zwelch <zwelchmobile@googlemail.com>
2023-10-23 09:29:15 +0200

    123

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,9 +1,14 @@
 { pkgs, lib, ... }:
-
+let
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/<branch or commit>)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+in
 {
   environment.systemPackages = with pkgs; [
     brave
     neofetch
-    eza
+    unstable.eza
   ];
 }
